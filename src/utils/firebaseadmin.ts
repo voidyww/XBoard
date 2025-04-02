26515fa31bc2492193787a03987df883857058fd
voidyww <yehiawael2005@gmail.com>
2025-04-02 16:01:44 +0200

    fix: switch to FIREBASE_ADMIN_SDK json-based config

--- src/utils/firebaseadmin.ts
+++ src/utils/firebaseadmin.ts
@@ -1,14 +1,12 @@
 // utils/firebaseAdmin.ts
 import admin from "firebase-admin";
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK || "{}");
+
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    }),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
-export const auth = admin.auth(); // <-- ✅ this line gives you `auth` to import
+export const auth = admin.auth();
