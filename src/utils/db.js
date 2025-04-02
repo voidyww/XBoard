dc0607857ad8c3c3a4327e174339d659d1d3b514
voidyww <yehiawael2005@gmail.com>
2025-04-02 19:46:09 +0200

    fix: use FIREBASE_ADMIN_SDK JSON for firebase-admin

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,12 +3,4 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "Lolzofive123!",
-    database: process.env.DB_NAME || "disboard",
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-});
+export const db = mysql.createPool(process.env.DATABASE_URL);
